8838d3764773f2b13a0377a6c144a241de3d70b0
agareaditya-8055 <adityaagare8055@gmail.com>
2026-01-09 23:06:21 +0530

    add

--- dummy-project/src/App.tsx
+++ dummy-project/src/App.tsx
@@ -4,7 +4,6 @@ import Corousal from "./components/Corousal"
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-800 flex justify-center items-center"><Corousal/></div> 
-    
   )
 }
 
